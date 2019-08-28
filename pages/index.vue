fe7a119fcb8b8edacad861f9f47b0a7112ddaac3
Chris Allan <christopher.john.allan@gmail.com>
2019-08-27 21:19:37 -0500

    Update text

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         ecommerce
       </h1>
       <h2 class="subtitle">
-        My glorious Nuxt.js project
+        My glorious Nuxt.js project :)
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
